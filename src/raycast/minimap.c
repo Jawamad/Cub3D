cd4d4b10f01356ad3fbaf5e34f66a8dbf257ecbf
Florian Muller <flmuller@c1r3p5.42perpignan.fr>
2024-09-09 17:43:19 +0200

    wip raycast

--- src/raycast/minimap.c
+++ src/raycast/minimap.c
@@ -2,49 +2,70 @@
 
 void	set_minimap(t_data *data)
 {
-	t_coord	*map_reader;
+	t_coord	*player;
+	t_coord	pencil;
 
-	map_reader = init_pos(0);
-	while ((map_reader.x <= data->map_data.width) && map_reader.y <= (data->map_data.height))
+	player = search_player(data);
+	pencil.x = player->x - 5;
+	pencil.y = player->y - 5;
+	if (pencil.x < 0)
+		pencil.x = 0;
+	if (pencil.y < 0)
+		pencil.y = 0;
+	while (pencil.y < (player->y + 5) && pencil.y < data->map_data.height)
 	{
-		// utiliser un buffer au lieu de put_pixel
-		if (data->map_data.map[map_reader.y][map_reader.x])
-		if 
-		mlx_pixel_put(data->mlx, data->mlx_win, pencil.x++, pencil.y, "#0000FF");
-		if (pencil.x == (data->map_data.height + 10) * 4 && pencil.y < (data->map_data.width + 10) *4)
+		while (pencil.x < (player->x + 5) && pencil.x < data->map_data.width)
 		{
-			pencil.x = 9;
-			pencil.y++;
+			put_tile_mmap(pencil.x, pencil.y, data);
+			pencil.x++;
 		}
+		pencil.y++;
 	}
-	//put_wall_mmap(data);
+	free(player);//a verifier
 }
 
-void	put_wall_mmap(t_data *data)
+void	put_tile_mmap(int x, int y, t_data *data)
 {
-	t_pos pencil;
-
-	pencil.x = 9;
-	pencil.y = 9;
+	if (!is_player_mmap(data->map_data.map[y][x]))
+		paint_tile_mmap(x, y, 0, data);
+	if (!is_wall_mmap(data->map_data.map[y][x]))
+		paint_tile_mmap(x, y, 1, data);
 }
 
-void	put_floor_mmap(t_data *data)
+void	paint_tile_mmap(int x, int y, int color,t_data *data)
 {
+	t_coord pencil;
 
+	pencil.x = x * 4;
+	pencil.y = y * 4;
+	while (pencil.y < y * 4 + 4)
+	{
+		while (pencil.x < x * 4 + 4)
+		{
+			mlx_pixel_put(data->mlx, data->mlx_win, pencil.x, pencil.y, color);
+			pencil.x++;
+		}
+		pencil.y++;
+	}
 }
 
-void	put_player_mmap(t_data *data)
+
+int	is_player_mmap(char c)
 {
-
+	if (c == 'N' || c == 'S' || c == 'E' || c == 'W')
+		return (0);
+	return(1);
 }
+
 
 t_coord	*search_player(t_data *data)
 {
 	t_coord *pos;
+
 	pos = init_pos(0);
 	while (data->map_data.map[pos->y][pos->x])
 	{
-		if (data->map_data.map[pos->y][pos->x] == 'P')
+		if (!is_player_mmap(data->map_data.map[pos->y][pos->x]))
 			return (pos);
 		pos->x++;
 		if (pos->x >= data->map_data.width)
